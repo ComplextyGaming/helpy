ededeecfba96fa7668f44bbbd178ada4986aab0c
LucianoLopez <lucianolopez4000@hotmail.com>
2021-09-19 16:23:51 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component'
+import { LoginComponent } from './components/login/login.component'
+import { RegisterComponent } from './components/register/register.component'
+import { ExpertComponent } from  './components/expert/expert.component'
+import { ProfileComponent } from './components/profile/profile.component'
+import { GamesComponent } from './components/games/games.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'games',
+    component: GamesComponent
+  },
+  {
+    path: 'expert',
+    component: ExpertComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule }  from '@angular/material/input'
+import { MatFormFieldModule }  from '@angular/material/form-field';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ExpertComponent } from './components/expert/expert.component'
 
 @NgModule({
+  exports: [
+
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProfileComponent,
+    ExpertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
